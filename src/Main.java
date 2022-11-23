81b6d35fdc34135f6dfe8fbe3cebf85c58d4ce13
Svetlana <isaeva322@mail.ru>
2022-11-23 17:20:28 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,22 @@ public class Main {
         System.out.println(" Задание 1");
 
         for ( int a = 0; a <= 10; a++){
-            System.out.println( a );
+        System.out.println( a );
         }
+
+
+
+        // Задание 2
+        System.out.println(" Задание 2");
+
+        for ( int b = 10; b >= 0 ; b--){
+            System.out.println( b );
+        }
+
+
+
+
+
     }
+
 }
